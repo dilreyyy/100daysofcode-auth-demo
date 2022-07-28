469f9cb07609f2b19a94f12aa51cf3947fbaec96
Its Ya Boi Del Rey <delreeyyy@gmail.com>
2022-07-29 00:31:03 +0800

    Basic validation for signup information

--- routes/demo.js
+++ routes/demo.js
@@ -1,65 +1,89 @@
-const express = require('express');
-const encrypt = require('bcryptjs');
+const express = require("express");
+const encrypt = require("bcryptjs");
 
-const db = require('../data/database');
+const db = require("../data/database");
 
 const router = express.Router();
 
-router.get('/', function (req, res) {
-  res.render('welcome');
+router.get("/", function (req, res) {
+  res.render("welcome");
 });
 
-router.get('/signup', function (req, res) {
-  res.render('signup');
+router.get("/signup", function (req, res) {
+  res.render("signup");
 });
 
-router.get('/login', function (req, res) {
-  res.render('login');
+router.get("/login", function (req, res) {
+  res.render("login");
 });
 
-router.post('/signup', async function (req, res) {
+router.post("/signup", async function (req, res) {
   const userData = req.body;
   const email = userData.email;
-  const confirmEmail = userData['confirm-email'];
+  const confirmEmail = userData["confirm-email"];
   const password = userData.password;
+  // console.log(password.trim().length);
+  if (
+    !email ||
+    !confirmEmail ||
+    !password ||
+    password.trim().length < 6 ||
+    email !== confirmEmail ||
+    !email.includes("@")
+  ) {
+    console.log("Invalid data");
+    return res.redirect("/signup");
+  }
+
+  const emailExisting = await db
+    .getDb()
+    .collection("users")
+    .findOne({ email: email });
+  
+  if (emailExisting) {
+    console.log("Email exists - try another one");
+    return res.redirect('/signup');
+  }
 
   const encryptedPassword = await encrypt.hash(password, 12);
 
   const user = {
     email: email,
-    password: encryptedPassword
-  }
+    password: encryptedPassword,
+  };
 
-  await db.getDb().collection('users').insertOne(user);
-  res.redirect('/login');
+  await db.getDb().collection("users").insertOne(user);
+  res.redirect("/login");
 });
 
-router.post('/login', async function (req, res) {
+router.post("/login", async function (req, res) {
   const userData = req.body;
   const email = userData.email;
   const password = userData.password;
 
-  const emailFound = await db.getDb().collection('users').findOne({email: email});
+  const emailFound = await db
+    .getDb()
+    .collection("users")
+    .findOne({ email: email });
 
   if (!emailFound) {
     console.log("Email does not match in our database!");
-    return res.redirect('/login');
+    return res.redirect("/login");
   }
   const passwordFound = await encrypt.compare(password, emailFound.password);
 
   if (!passwordFound) {
-    console.log('Password is incorrect!');
-    return res.redirect('/login');
+    console.log("Password is incorrect!");
+    return res.redirect("/login");
   }
 
-  console.log('Success');
-
+  console.log("Success");
 });
 
-router.get('/admin', function (req, res) {
-  res.render('admin');
+router.get("/admin", function (req, res) {
+  res.render("admin");
 });
 
-router.post('/logout', function (req, res) {});
+router.post("/logout", function (req, res) {});
 
 module.exports = router;
